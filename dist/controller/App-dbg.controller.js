832db2c926c609669cc4e09ed4a767435c711ec8
HikmahDevStudio <hikmahdevstudio@gmail.com>
2025-10-23 08:48:47 +0530

    Updated

---
+++ dist/controller/App-dbg.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("hds.ui5.todolistBasic.controller.App", {
+    onInit() {},
+  });
+});
